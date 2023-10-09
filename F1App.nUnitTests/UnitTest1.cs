588e7e167c8c9fe5f04725976e70d0f492a13517
aniketsabnis <aniket.n.sabnis@gmail.com>
2023-10-09 13:09:22 +0530

    Adding tests

---
+++ F1App.nUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace F1App.nUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetDriverNames_Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
